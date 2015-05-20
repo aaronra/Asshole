4e4aa3c45dd0c5fc13c54b3ef7ebf178fe2ba2b6
t0tep <neon0619@gmail.com>
2015-05-20 11:16:35 +0800

    layoutproblems

--- StandTracker/UserInfoViewController.swift
+++ StandTracker/UserInfoViewController.swift
@@ -11,12 +11,25 @@ import UIKit
 class UserInfoViewController: UIViewController {
 
     
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet var uiView: UIView!
 
+    @IBOutlet weak var txtQone: UITextField!
+    @IBOutlet weak var txtQtwo: UITextField!
+    @IBOutlet weak var txtQthree: UITextField!
+    @IBOutlet weak var txtQfour: UITextField!
+    @IBOutlet weak var txtQfive: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         allAboutUI()
+        
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "hideKeyboard")
+        // prevents the scroll view from swallowing up the touch event of child buttons
+        tapGesture.cancelsTouchesInView = false
+        scrollView.addGestureRecognizer(tapGesture)
+        
     }
     
     
@@ -28,5 +41,52 @@ class UserInfoViewController: UIViewController {
         imageView.frame = CGRect(x: 0, y: 0, width: 10, height: 25)
         navigationItem.titleView = imageView
     }
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(true)
+        self.registerForKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        self.deregisterFromKeyboardNotifications()
+        super.viewWillDisappear(true)
+        
+    }
+    
+    func registerForKeyboardNotifications() -> Void {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWasShown:", name: UIKeyboardDidShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWasShown:", name: UIKeyboardWillShowNotification, object: nil)
+    }
+    
+    func deregisterFromKeyboardNotifications() -> Void {
+        println("Deregistering!")
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWasShown:", name: UIKeyboardDidHideNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWasShown:", name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func keyboardWasShown(notification: NSNotification) {
+        var info: Dictionary = notification.userInfo!
+        var keyboardSize: CGSize = (info[UIKeyboardFrameBeginUserInfoKey]?.CGRectValue().size)!
+        var buttonOrigin: CGPoint = self.txtQfive.frame.origin;
+        var buttonHeight: CGFloat = self.txtQfive.frame.size.height;
+        var visibleRect: CGRect = self.view.frame
+        visibleRect.size.height -= keyboardSize.height
+        
+        if (!CGRectContainsPoint(visibleRect, buttonOrigin)) {
+            var scrollPoint: CGPoint = CGPointMake(0.0, buttonOrigin.y - visibleRect.size.height + buttonHeight + 4)
+            self.scrollView.setContentOffset(scrollPoint, animated: true)
+        }
+    }
+    
+    func hideKeyboard() {
+        txtQone.resignFirstResponder()   //FirstResponder's must be resigned for hiding keyboard.
+        txtQtwo.resignFirstResponder()
+        txtQthree.resignFirstResponder()
+        txtQfour.resignFirstResponder()
+        txtQfive.resignFirstResponder()
+        self.scrollView.setContentOffset(CGPointZero, animated: true)
+    }
+
 
 }
